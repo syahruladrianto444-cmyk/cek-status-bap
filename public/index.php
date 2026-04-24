580e33dad1ff177aa784239810614f302b7d2f62
syhadr <syahruladrianto444@gmail.com>
2026-04-24 19:08:46 +0700

    Inject env vars in public/index.php

--- public/index.php
+++ public/index.php
@@ -1,5 +1,31 @@
 <?php
 
+// Force production environment and TiDB settings for Vercel
+if (getenv('VERCEL')) {
+    $env = [
+        'APP_ENV' => 'production',
+        'APP_DEBUG' => 'false',
+        'LOG_CHANNEL' => 'stderr',
+        'SESSION_DRIVER' => 'cookie',
+        'CACHE_DRIVER' => 'array',
+        'VIEW_COMPILED_PATH' => '/tmp',
+        'DB_CONNECTION' => 'mysql',
+        'DB_HOST' => 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com',
+        'DB_PORT' => '4000',
+        'DB_DATABASE' => 'test',
+        'DB_USERNAME' => '3HmEguLRggppLdM.root',
+        'DB_PASSWORD' => 'bjihRFNq2ZQgh0Yz',
+        'MYSQL_ATTR_SSL_CA' => '/etc/pki/tls/certs/ca-bundle.crt',
+    ];
+
+    foreach ($env as $key => $value) {
+        putenv("$key=$value");
+        $_ENV[$key] = $value;
+        $_SERVER[$key] = $value;
+    }
+}
+
+
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 

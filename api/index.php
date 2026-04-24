735d0d02e97bd69f65acf38f70d8e49acd50aa12
syhadr <syahruladrianto444@gmail.com>
2026-04-24 19:01:36 +0700

    Inject env vars in api/index.php for Vercel stability

--- api/index.php
+++ api/index.php
@@ -1,3 +1,11 @@
 <?php
 
+// Ensure production environment on Vercel
+putenv('APP_ENV=production');
+putenv('APP_DEBUG=false');
+putenv('LOG_CHANNEL=stderr');
+putenv('SESSION_DRIVER=cookie');
+putenv('CACHE_DRIVER=array');
+putenv('VIEW_COMPILED_PATH=/tmp');
+
 require __DIR__ . '/../public/index.php';
